77776e0a8aaccf9a078c29a1572e16c383c0fac5
pswiss <petrasswissler2022@u.northwestern.edu>
2017-04-27 08:27:57 -0500

    Updated Code to what was on github

--- HW9_Code/firmware/src/system_config/default/system_config.h
+++ HW9_Code/firmware/src/system_config/default/system_config.h
@@ -19,7 +19,7 @@
     until used by another MPLAB Harmony module or application.
 
     Created with MPLAB Harmony Version 1.10
-*******************************************************************************/
+ *******************************************************************************/
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
@@ -43,7 +43,7 @@ INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
 CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
 SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
-*******************************************************************************/
+ *******************************************************************************/
 // DOM-IGNORE-END
 
 #ifndef _SYSTEM_CONFIG_H
@@ -56,7 +56,7 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 /*  This section Includes other configuration headers necessary to completely
     define this configuration.
-*/
+ */
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -64,55 +64,111 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 extern "C" {
 
 #endif
-// DOM-IGNORE-END
+    // DOM-IGNORE-END
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: System Service Configuration
-// *****************************************************************************
-// *****************************************************************************
-// *****************************************************************************
-/* Common System Service Configuration Options
-*/
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: System Service Configuration
+    // *****************************************************************************
+    // *****************************************************************************
+    // *****************************************************************************
+    /* Common System Service Configuration Options
+     */
 #define SYS_VERSION_STR           "1.10"
 #define SYS_VERSION               11000
 
-// *****************************************************************************
-/* Clock System Service Configuration Options
-*/
+    // *****************************************************************************
+    /* Clock System Service Configuration Options
+     */
 #define SYS_CLK_FREQ                        48000000ul
 #define SYS_CLK_BUS_PERIPHERAL_1            48000000ul
-#define SYS_CLK_UPLL_BEFORE_DIV2_FREQ       96000000ul
+#define SYS_CLK_UPLL_BEFORE_DIV2_FREQ       63999984ul
 #define SYS_CLK_CONFIG_PRIMARY_XTAL         8000000ul
 #define SYS_CLK_CONFIG_SECONDARY_XTAL       0ul
-   
-/*** Ports System Service Configuration ***/
+
+    /*** Interrupt System Service Configuration ***/
+#define SYS_INT                     true
+
+    /*** Ports System Service Configuration ***/
+
+#define SYS_PORT_B_ANSEL        0xac1f
+#define SYS_PORT_B_TRIS         0xefbf
+#define SYS_PORT_B_LAT          0x0
+#define SYS_PORT_B_ODC          0x0
+#define SYS_PORT_B_CNPU         0x0
+#define SYS_PORT_B_CNPD         0x0
+#define SYS_PORT_B_CNEN         0x0
 
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Driver Configuration
-// *****************************************************************************
-// *****************************************************************************
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Driver Configuration
+    // *****************************************************************************
+    // *****************************************************************************
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Middleware & Other Library Configuration
-// *****************************************************************************
-// *****************************************************************************
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Middleware & Other Library Configuration
+    // *****************************************************************************
+    // *****************************************************************************
 
+    /*** USB Driver Configuration ***/
 
+    /* Enables Device Support */
+#define DRV_USBFS_DEVICE_SUPPORT      true
 
+    /* Disable Device Support */
+#define DRV_USBFS_HOST_SUPPORT      false
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Configuration
-// *****************************************************************************
-// *****************************************************************************
+    /* Maximum USB driver instances */
+#define DRV_USBFS_INSTANCES_NUMBER    1
 
-/*** Application Instance 0 Configuration ***/
+    /* Interrupt mode enabled */
+#define DRV_USBFS_INTERRUPT_MODE      true
 
-//DOM-IGNORE-BEGIN
+    /* Number of Endpoints used */
+#define DRV_USBFS_ENDPOINTS_NUMBER    3 // was 1
+
+    /*** USB Device Stack Configuration ***/
+
+    /* The USB Device Layer will not initialize the USB Driver */
+#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
+
+    /* Maximum device layer instances */
+#define USB_DEVICE_INSTANCES_NUMBER     1
+
+    /* EP0 size in bytes */
+#define USB_DEVICE_EP0_BUFFER_SIZE      8 // was 64
+
+    /* Enable SOF Events */
+#define USB_DEVICE_SOF_EVENT_ENABLE     
+
+    /* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER     1
+
+    /* CDC Transfer Queue Size for both read and
+       write. Applicable to all instances of the
+       function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED 3
+
+    /* Application USB Device CDC Read Buffer Size. This should be a multiple of
+     * the CDC Bulk Endpoint size */
+
+#define APP_READ_BUFFER_SIZE 64
+
+    /* Macro defines USB internal DMA Buffer criteria*/
+
+#define APP_MAKE_BUFFER_DMA_READY
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Application Configuration
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*** Application Instance 0 Configuration ***/
+
+    //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
 }
 #endif
@@ -122,5 +178,5 @@ extern "C" {
 #endif // _SYSTEM_CONFIG_H
 /*******************************************************************************
  End of File
-*/
+ */
 
